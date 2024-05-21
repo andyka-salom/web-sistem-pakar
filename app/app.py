4c4cec7dc5b8107370eefa7f609892f21a323116
andyka-salom <andykasalom45@gmail.com>
2024-05-22 02:02:07 +0700

    seed blm selesai

--- app/app.py
+++ app/app.py
@@ -1,23 +1,30 @@
 from flask import Flask, render_template, request
-from models import db, Trait, Personality, ExpertSystem  # Pastikan model diimport dengan benar
+from models import db, Trait, Personality, ExpertSystem, Penyakit, Gejala, Keputusan  # Pastikan model diimport dengan benar
 from config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config)
 db.init_app(app)
 
-@app.route('/', methods=['GET', 'POST'])
-def index():
+@app.route('/')
+def landing_page():
+    return render_template('index.html')
+
+@app.route('/personality', methods=['GET', 'POST'])
+def personality():
     if request.method == 'POST':
         selected_traits = request.form.getlist('traits')
         if selected_traits:
             # Find the personality based on the selected traits
             result = get_personality(selected_traits)
-            return render_template('index.html', traits=get_traits(), result=result)
-    return render_template('index.html', traits=get_traits())
+            return render_template('personality.html', traits=get_traits(), result=result)
+    return render_template('personality.html', traits=get_traits())
 
 def get_traits():
     return Trait.query.all()
+
+def get_gejala():
+    return Gejala.query.all()
 
 def get_personality(trait_codes):
     personalities = Personality.query.all()
@@ -29,6 +36,35 @@ def get_personality(trait_codes):
         if set(trait_ids) == set(expert_system_trait_ids):
             return personality.name
     return "Kepribadian tidak ditemukan"
+
+
+
+
+
+
+
+@app.route('/penyakit', methods=['GET', 'POST'])
+def penyakit():
+    if request.method == 'POST':
+        selected_gejala = request.form.getlist('gejala')
+        if selected_gejala:
+            # Find the disease based on the selected symptoms
+            result = get_penyakit(selected_gejala)
+            return render_template('penyakit.html', gejala=get_gejala(), result=result)
+    return render_template('penyakit.html', gejala=get_gejala())
+
+
+
+def get_penyakit(gejala_codes):
+    penyakit_list = Penyakit.query.all()
+    for penyakit in penyakit_list:
+        gejala_ids = [gejala.id for gejala in Gejala.query.filter(Gejala.code.in_(gejala_codes)).all()]
+        keputusan_entries = Keputusan.query.filter_by(penyakit_id=penyakit.id).all()
+        keputusan_gejala_ids = [entry.gejala_id for entry in keputusan_entries]
+
+        if set(gejala_ids) == set(keputusan_gejala_ids):
+            return penyakit.nama
+    return "Penyakit tidak ditemukan"
 
 if __name__ == '__main__':
     with app.app_context():
--- app/models.py
+++ app/models.py
@@ -19,3 +19,33 @@ class ExpertSystem(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     personality_id = db.Column(db.Integer, db.ForeignKey('personalities.id'), nullable=False)
     trait_id = db.Column(db.Integer, db.ForeignKey('traits.id'), nullable=False)
+
+class Penyakit(db.Model):
+    __tablename__ = 'penyakit'
+    id = db.Column(db.Integer, primary_key=True)
+    kode = db.Column(db.String(10), nullable=False)
+    nama = db.Column(db.String(100), nullable=False)
+
+class Gejala(db.Model):
+    __tablename__ = 'gejala'
+    id = db.Column(db.Integer, primary_key=True)
+    kode = db.Column(db.String(10), nullable=False)
+    nama = db.Column(db.String(100), nullable=False)
+
+class Keputusan(db.Model):
+    __tablename__ = 'keputusan_data'
+    id = db.Column(db.Integer, primary_key=True)
+    kode_penyakit = db.Column(db.String(10), nullable=False)
+    kode_gejala = db.Column(db.String(255), nullable=False)  # Perubahan panjang kolom kode_gejala
+    penyakit_id = db.Column(db.Integer, db.ForeignKey('penyakit.id'))
+    gejala_id = db.Column(db.Integer, db.ForeignKey('gejala.id'))
+
+class NilaiCFGejala(db.Model):
+    __tablename__ = 'nilai_cf_gejala'
+    id = db.Column(db.Integer, primary_key=True)
+    penyakit_id = db.Column(db.Integer, db.ForeignKey('penyakit.id'), nullable=False)
+    gejala_id = db.Column(db.Integer, db.ForeignKey('gejala.id'), nullable=False)
+    nilai_cf = db.Column(db.Float, nullable=False)
+
+    penyakit = db.relationship('Penyakit', backref=db.backref('nilai_cf_gejala', lazy=True))
+    gejala = db.relationship('Gejala', backref=db.backref('nilai_cf_gejala', lazy=True))
